1072412276b51c2db183278d4346ed7497e80981
ASHuenchuleo <arsepulh@gmail.com>
2023-06-30 23:51:10 -0400

    2.0

--- public/admin.js
+++ public/admin.js
@@ -1,22 +1,13 @@
 'use strict';
-/* globals $, app */
 
-define('admin/plugins/slick', ['settings'], function(Settings) {
-
+define('admin/plugins/vtubeando', ['settings'], function (Settings) {
 	var ACP = {};
 
-	ACP.init = function() {
-		Settings.load('slick', $('.slick-settings'));
+	ACP.init = function () {
+		Settings.load('vtubeando', $('.vtubeando-settings'));
 
-		$('#save').on('click', function() {
-			Settings.save('slick', $('.slick-settings'), function() {
-				app.alert({
-					type: 'success',
-					alert_id: 'slick-saved',
-					title: 'Settings Saved',
-					message: 'Slick theme settings saved'
-				});
-			});
+		$('#save').on('click', function () {
+			Settings.save('vtubeando', $('.vtubeando-settings'));
 		});
 	};
 
